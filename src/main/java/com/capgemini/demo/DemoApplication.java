482242237d0d4065010777bd131ad478b2e9c1d3
Yashar Kalantari <yashar.k@gmail.com>
2019-10-11 14:48:08 +0200

    final added to method signature

--- src/main/java/com/capgemini/demo/DemoApplication.java
+++ src/main/java/com/capgemini/demo/DemoApplication.java
@@ -5,12 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	
-	private DemoApplication() {
 		
-	}
-	
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
