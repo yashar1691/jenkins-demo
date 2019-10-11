618ed54bd8f748f2c5e4b3bfe78e3c5fa4085999
Yashar Kalantari <yashar.k@gmail.com>
2019-10-11 14:44:57 +0200

    private constructor added

--- src/main/java/com/capgemini/demo/DemoApplication.java
+++ src/main/java/com/capgemini/demo/DemoApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	
+	private DemoApplication() {
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
